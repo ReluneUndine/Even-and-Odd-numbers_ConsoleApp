3a311ece6c327ab60f9f7526ab2987753084570f
ReluneUndine <kanashimuonna@yandex.ru>
2024-02-23 03:33:29 +0300

    Добавьте файлы проекта.

---
+++ Even and Odd numbers_ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_and_Odd_numbers_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Точка входа, начало кода
+        {
+            Console.WriteLine("Приложение по определению чётного или нечётного числа");
+
+            for (; ; ) // использую бесконечный цикл (без счетчика), чтобы игра никогда не заканчивалась
+            {
+                Console.WriteLine("\n\n Пожалуйста, введите целое число: "); // использую вместе с WriteLine дополнительный перенос строк \n\n (увеличиваю расстояние)
+                int integer = Convert.ToInt32(Console.ReadLine()); // конвертирую введенное пользователем строковое значение -> в числовое
+
+                if (integer % 2 == 0) // использую оператор деления с остатком %, чтобы понять, четное число или нет
+                {
+                    Console.Write($"{integer} - это чётное число.");
+                }
+                else
+                {
+                    Console.Write($"{integer} - это нечётное число.");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
